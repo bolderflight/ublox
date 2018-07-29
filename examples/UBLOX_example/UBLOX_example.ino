6096756f4ac903adca80623cba5fe0890a74beb8
Brian Taylor <brian.taylor@bolderflight.com>
2018-07-29 11:47:25 -0700

    Updated example

--- examples/UBLOX_example/UBLOX_example.ino
+++ examples/UBLOX_example/UBLOX_example.ino
@@ -2,7 +2,6 @@
 UBLOX_example.ino
 Brian R Taylor
 brian.taylor@bolderflight.com
-2016-07-06
 
 Copyright (c) 2016 Bolder Flight Systems
 
@@ -24,48 +23,42 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
 
 #include "UBLOX.h"
 
-// a uBlox object, which is on Teensy hardware
-// serial port 3
-UBLOX gps(1);
-
-// the uBlox data structure, UBX-NAV-PVT
-gpsData uBloxData;
+// a uBlox object, which is on hardware
+// serial port 1 with a baud rate of 115200
+UBLOX gps(Serial1,115200);
 
 void setup() {
   // serial to display data
-  Serial.begin(9600);
+  Serial.begin(115200);
 
-  // starting communication with the GPS
-  // receiver at 115200 baud
-  gps.begin(115200);
-  
+  // starting communication with the GPS receiver
+  gps.begin();
 }
 
 void loop() {
-
   // checking to see if a good packet has
   // been received and displaying some
   // of the packet data
-  if( gps.read(&uBloxData) ){
-    Serial.print(uBloxData.utcYear);  ///< [year], Year (UTC)
+  if(gps.readSensor()) {
+    Serial.print(gps.getYear());                ///< [year], Year (UTC)
     Serial.print("\t");
-    Serial.print(uBloxData.utcMonth); ///< [month], Month, range 1..12 (UTC)
+    Serial.print(gps.getMonth());               ///< [month], Month, range 1..12 (UTC)
     Serial.print("\t");
-    Serial.print(uBloxData.utcDay);   ///< [day], Day of month, range 1..31 (UTC)
+    Serial.print(gps.getDay());                 ///< [day], Day of month, range 1..31 (UTC)
     Serial.print("\t");
-    Serial.print(uBloxData.utcHour);  ///< [hour], Hour of day, range 0..23 (UTC)
+    Serial.print(gps.getHour());                ///< [hour], Hour of day, range 0..23 (UTC)
     Serial.print("\t");
-    Serial.print(uBloxData.utcMin);   ///< [min], Minute of hour, range 0..59 (UTC)
+    Serial.print(gps.getMin());                 ///< [min], Minute of hour, range 0..59 (UTC)
     Serial.print("\t");
-    Serial.print(uBloxData.utcSec);   ///< [s], Seconds of minute, range 0..60 (UTC)
+    Serial.print(gps.getSec());                 ///< [s], Seconds of minute, range 0..60 (UTC)
     Serial.print("\t");
-    Serial.print(uBloxData.numSV);    ///< [ND], Number of satellites used in Nav Solution
+    Serial.print(gps.getNumSatellites());       ///< [ND], Number of satellites used in Nav Solution
     Serial.print("\t");
-    Serial.print(uBloxData.lat,10);   ///< [deg], Latitude
+    Serial.print(gps.getLatitude_deg(),10);     ///< [deg], Latitude
     Serial.print("\t");
-    Serial.print(uBloxData.lon,10);   ///< [deg], Longitude
+    Serial.print(gps.getLongitude_deg(),10);    ///< [deg], Longitude
     Serial.print("\t");
-    Serial.println(uBloxData.hMSL);   ///< [m], Height above mean sea level
+    Serial.println(gps.getMSLHeight_ft());      ///< [ft], Height above mean sea level
   }
 }
 
