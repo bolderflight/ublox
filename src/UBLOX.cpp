698750412cc09cf14a11328840de5c73279b0613
jacky4566 <jacksonwiebe@me.com>
2019-03-28 09:37:34 -0600

    Added functions for age

--- src/UBLOX.cpp
+++ src/UBLOX.cpp
@@ -40,14 +40,29 @@ void UBLOX::begin()
 	_bus->begin(_baud);
 }
 
+/* clear Status Flags */
+void UBLOX::clearStatus()
+{
+	// initialize parsing state
+	_validPacket.valid = 0;
+	_validPacket.flags = 0;
+}
+
 /* reads packets from the uBlox receiver */
 bool UBLOX::readSensor()
 {
 	if (_parse(_ubxNavPvt_msgClass,_ubxNavPvt_msgId,_ubxNavPvt_msgLen)) {
+		validPacketAge = millis();
 		return true;
 	} else {
 		return false;
 	}
+}
+
+/* returns timestamp of last valid packet*/
+unsigned long UBLOX::age()
+{
+	return validPacketAge;
 }
 
 /* GPS time of week of nav solution, ms */
